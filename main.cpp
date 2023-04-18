b63ac3fb9c911e53576bfe93b7b5d22476aa600d
Tadeo-Guerstein <tadeleonino@hotmail.com>
2023-04-18 20:32:10 -0300

    cambio el codigo

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     int respuesta;
     cout<<"Hola mundo!";
-    respuesta = 34 * 2;
+    respuesta = 34 * 45;
     cout<<"Hola";
     cout << respuesta;
     return 0;
