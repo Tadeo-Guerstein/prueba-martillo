e02a13cfb39b743c6ab6b12a918717d42d91e4e4
machadosandra1986 <machadosandra1986@gmail.com>
2023-04-18 20:10:05 -0300

    agrego objeto martillo

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int respuesta;
+    cout<<"Hola mundo!";
+    respuesta = 34 * 2;
+    cout << respuesta;
+    return 0;
+}
